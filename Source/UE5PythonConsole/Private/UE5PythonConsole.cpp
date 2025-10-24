0b2a9722d5d4fc5abd766867792b99414b454d75
kasicass <kasicass@gmail.com>
2025-10-24 20:58:21 +0800

    init UE5PythonConsole

---
+++ Source/UE5PythonConsole/Private/UE5PythonConsole.cpp
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5PythonConsole.h"
+
+#define LOCTEXT_NAMESPACE "FUE5PythonConsoleModule"
+
+void FUE5PythonConsoleModule::StartupModule()
+{
+}
+
+void FUE5PythonConsoleModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FUE5PythonConsoleModule, UE5PythonConsole)
---
+++ Source/UE5PythonConsole/UE5PythonConsole.Build.cs
@@ -0,0 +1,44 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.IO;
+
+public class UE5PythonConsole : ModuleRules
+{
+	public UE5PythonConsole(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				// ... add other private include paths required here ...
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+			}
+			);
+	}
+}
